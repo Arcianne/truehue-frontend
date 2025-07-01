7eacb372e519bc8eac46177df442c70ca1accca6
siridc <dessireecamillepasion65@gmail.com>
2025-07-02 01:30:17 +0800

    Implement reusable CustomButton widget with icon

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 // import 'package:truehue/features/color_blindness_type_selector/presentation/pages/color_blindness_type_selector.dart';
 import 'package:truehue/features/welcome/presentation/pages/welcome.dart';
 
+import 'package:truehue/shared/presentation/widgets/button.dart';
+
+
 class Home extends StatelessWidget {
   const Home({ super.key });
 
@@ -29,65 +32,163 @@ class Home extends StatelessWidget {
   _buildAppbar() {
     return AppBar(
       title: const Text(
-        'home page',
+        'Home Page',
       )
     );
   }
 
-  _buildBody(BuildContext context) {
-    return Center(
+ _buildBody(BuildContext context) {
+  return SafeArea(
+    child: Center(
       child: Column(
         children: <Widget>[
-          Image.asset('assets/icon/truehue_logo.png', width: 100, height: 100),
-          const SizedBox(height: 20),
+          Container(
+            margin: const EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
+            child: Image.asset(
+              'assets/icon/truehue_logo.png',
+              width: 150,
+              height: 150,
+            ),
+          ),
+
           const Text(
             'TrueHue',
             style: TextStyle(
               color: Color(0xFFCEF5FF),
-              fontSize: 24,
+              fontSize: 30,
               fontStyle: FontStyle.italic,
             ),
           ),
 
           const SizedBox(height: 20),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFCEF5FF),
-              foregroundColor: Color(0xFF130E64),
-            ),
-            onPressed: () {},
-            child: const Text('AR Live View'),
+
+          CustomButton(
+            title: 'AR Live View',
+            icon: Icons.visibility,
+            iconSize: 24,
+            softWrap: false,
+            fontSize: 25,
+            height: 60,
+            width: 260,
+            onPressed: () {
+              Navigator.push(
+                _buildBody(context),
+                MaterialPageRoute(builder: (context) => Home()), //change to AR Live View page
+              );
+            },
           ),
 
           const SizedBox(height: 20),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFCEF5FF),
-              foregroundColor: Color(0xFF130E64),
-            ),
-            onPressed: () {},
-            child: const Text('Take Photo'),
+
+          CustomButton(
+            title: 'Take Photo',
+            icon: Icons.camera_alt,
+            iconSize: 24,
+            softWrap: false,
+            fontSize: 25,
+            height: 60,
+            width: 260,
+            onPressed: () {
+              Navigator.push(
+                _buildBody(context),
+                MaterialPageRoute(builder: (context) => Home()), //change to Take Photo page
+              );
+            },
           ),
 
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFCEF5FF),
-              foregroundColor: Color(0xFF130E64),
-            ),
-            onPressed: () {},
-            child: const Text('Select Photo'),
+          const SizedBox(height: 20),
+
+          CustomButton(
+            title: 'Select Photo',
+            icon: Icons.photo_library,
+            iconSize: 24,
+            softWrap: false,
+            fontSize: 25,
+            height: 60,
+            width: 260,
+            onPressed: () {
+              Navigator.push(
+                _buildBody(context),
+                MaterialPageRoute(builder: (context) => Home()), //change to Select Photo page
+              );
+            },
           ),
 
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFCEF5FF),
-              foregroundColor: Color(0xFF130E64),
-            ),
-            onPressed: () {},
-            child: const Text('Color Library'),
+          const SizedBox(height: 20),
+
+          CustomButton(
+            title: 'Color Library',
+            icon: Icons.color_lens,
+            iconSize: 24,
+            softWrap: false,
+            fontSize: 25,
+            height: 60,
+            width: 260,
+            onPressed: () {
+              Navigator.push(
+                _buildBody(context),
+                MaterialPageRoute(builder: (context) => Home()),//change to Color Library page
+              );
+            },
           ),
+
+
+    //       const SizedBox(height: 50),
+    //       ElevatedButton(
+    //         style: ElevatedButton.styleFrom(
+    //           backgroundColor: Color(0xFFCEF5FF),
+    //           foregroundColor: Color(0xFF130E64),
+    //           // fixedSize: const Size(85, 31),
+    //           shape: RoundedRectangleBorder(
+    //           borderRadius: BorderRadius.circular(10),
+    //         ),
+    //         ),
+    //         onPressed: () {},
+    //         child: const Text('AR Live View',
+    //         style: TextStyle(fontSize: 22),
+    //         ),
+    //       ),
+
+    //       // const SizedBox(height: 20),
+    //       ElevatedButton(
+    //         style: ElevatedButton.styleFrom(
+    //           backgroundColor: Color(0xFFCEF5FF),
+    //           foregroundColor: Color(0xFF130E64),
+    //         ),
+    //         onPressed: () {},
+    //         child: const Text('Take Photo',
+    //         style: TextStyle(fontSize: 22),
+    //         ),
+    //       ),
+
+    //       ElevatedButton(
+    //         style: ElevatedButton.styleFrom(
+    //           backgroundColor: Color(0xFFCEF5FF),
+    //           foregroundColor: Color(0xFF130E64),
+    //         ),
+    //         onPressed: () {},
+    //         child: const Text('Select Photo',
+    //         style: TextStyle(fontSize: 22),
+    //         ),
+    //       ),
+
+    //       ElevatedButton(
+    //         style: ElevatedButton.styleFrom(
+    //           backgroundColor: Color(0xFFCEF5FF),
+    //           foregroundColor: Color(0xFF130E64),
+    //         ),
+    //         onPressed: () {},
+    //         child: const Text('Color Library',
+    //         style: TextStyle(fontSize: 22),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // ));
+  // }
         ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
