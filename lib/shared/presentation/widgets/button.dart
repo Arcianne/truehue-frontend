9435b72b2ad881c8cf85bdd234878e403b8c555d
siridc <dessireecamillepasion65@gmail.com>
2025-07-03 01:05:54 +0800

    removed the imports and class of ar live view

--- lib/shared/presentation/widgets/button.dart
+++ lib/shared/presentation/widgets/button.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:truehue/features/ar_live_view/presentation/pages/ar_live_view_page.dart';
+
+
 class CustomButton extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
@@ -9,6 +12,7 @@ class CustomButton extends StatelessWidget {
   final IconData? icon;
   final double? iconSize;
   final bool softWrap;
+  final double? padding;
 
   const CustomButton({
     super.key,
@@ -20,31 +24,8 @@ class CustomButton extends StatelessWidget {
     this.iconSize,
     this.fontSize,
     this.softWrap = true,
+    this.padding,
   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       height: height ?? 40,
-//       width: width ?? 85,
-//       child: ElevatedButton.icon(
-//         icon: icon != null ? Icon(icon) : const SizedBox.shrink(),
-//         label: Text(title,
-//           style:TextStyle(fontSize: fontSize ?? 19),
-//         ),
-//         style: ElevatedButton.styleFrom(
-//           backgroundColor: const Color(0xFFCEF5FF),
-//           foregroundColor: const Color(0xFF130E64),
-//           fixedSize: Size(width ?? 85, height ?? 31),
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(10),
-//           ),
-//         ),
-//         onPressed: onPressed,
-//       ),
-//     );
-//   }
-// }
 
     @override
     Widget build(BuildContext context) {
@@ -74,3 +55,10 @@ class CustomButton extends StatelessWidget {
         );
       }
     }
+
+void openARLiveView(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const ARLiveViewPage()),
+  );
+}
