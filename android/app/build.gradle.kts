9bbefea99836af1b1324047ec267dd6d4e02e7fd
siridc <dessireecamillepasion65@gmail.com>
2025-07-02 15:16:02 +0800

    remove sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,8 @@ android {
     namespace = "com.example.truehue"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    
+    // ndkVersion = "27.0.12077973"
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
